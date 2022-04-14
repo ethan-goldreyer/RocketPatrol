acecbb3b9ca09c64c1f248653090c56a3ee4e6a8
Ethan Goldreyer <egoldrey@ucsc.edu>
2022-04-13 18:07:35 -0700

    final build

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,23 @@
+// space ship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super (scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.moveSpeed = game.settings.spaceshipSpeed;
+    }
+
+    update() {
+        // move ship left
+        this.x -= this.MoveSpeed;
+        // wrap around
+        if (this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+
+    // reset function
+    reset() {
+        this.x = game.config.width;
+    }
+}
